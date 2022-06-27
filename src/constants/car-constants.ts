c772b43c84e824c66719054a30583302c6f85754
Hugo Moran <moranh56@gmail.com>
2022-06-26 18:20:46 -0600

    Add car game object constants

---
+++ src/constants/car-constants.ts
@@ -0,0 +1,40 @@
+import { CarId } from '../types/CarId';
+import { CarSpriteLoadData } from '../types/CarSpriteLoadData';
+
+export const CAR_OBJECT_NAMES: Record<CarId, string> = {
+    X: 'x-car-obj',
+    A: 'a-car-obj',
+    B: 'b-car-obj',
+    C: 'c-car-obj',
+    D: 'd-car-obj',
+    E: 'e-car-obj',
+    F: 'f-car-obj',
+    G: 'g-car-obj',
+    H: 'h-car-obj',
+    I: 'i-car-obj',
+    J: 'j-car-obj',
+    K: 'k-car-obj',
+    O: 'o-car-obj',
+    P: 'p-car-obj',
+    Q: 'q-car-obj',
+    R: 'r-car-obj'
+};
+
+export const CAR_SPRITE_DATA: Record<CarId, CarSpriteLoadData> = {
+    X: { name: 'x-car-sprite', horizontalSprite: 'horizontal-x-car.png', verticalSprite: 'vertical-x-car.png' },
+    A: { name: 'a-car-sprite', horizontalSprite: 'horizontal-a-car.png', verticalSprite: 'vertical-a-car.png' },
+    B: { name: 'b-car-sprite', horizontalSprite: 'horizontal-b-car.png', verticalSprite: 'vertical-b-car.png' },
+    C: { name: 'c-car-sprite', horizontalSprite: 'horizontal-c-car.png', verticalSprite: 'vertical-c-car.png' },
+    D: { name: 'd-car-sprite', horizontalSprite: 'horizontal-d-car.png', verticalSprite: 'vertical-d-car.png' },
+    E: { name: 'e-car-sprite', horizontalSprite: 'horizontal-e-car.png', verticalSprite: 'vertical-e-car.png' },
+    F: { name: 'f-car-sprite', horizontalSprite: 'horizontal-f-car.png', verticalSprite: 'vertical-f-car.png' },
+    G: { name: 'g-car-sprite', horizontalSprite: 'horizontal-g-car.png', verticalSprite: 'vertical-g-car.png' },
+    H: { name: 'h-car-sprite', horizontalSprite: 'horizontal-h-car.png', verticalSprite: 'vertical-h-car.png' },
+    I: { name: 'i-car-sprite', horizontalSprite: 'horizontal-i-car.png', verticalSprite: 'vertical-i-car.png' },
+    J: { name: 'j-car-sprite', horizontalSprite: 'horizontal-j-car.png', verticalSprite: 'vertical-j-car.png' },
+    K: { name: 'k-car-sprite', horizontalSprite: 'horizontal-k-car.png', verticalSprite: 'vertical-k-car.png' },
+    O: { name: 'o-car-sprite', horizontalSprite: 'horizontal-o-car.png', verticalSprite: 'vertical-o-car.png' },
+    P: { name: 'p-car-sprite', horizontalSprite: 'horizontal-p-car.png', verticalSprite: 'vertical-p-car.png' },
+    Q: { name: 'q-car-sprite', horizontalSprite: 'horizontal-q-car.png', verticalSprite: 'vertical-q-car.png' },
+    R: { name: 'r-car-sprite', horizontalSprite: 'horizontal-r-car.png', verticalSprite: 'vertical-r-car.png' }
+};
