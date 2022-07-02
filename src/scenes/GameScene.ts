6713188200ca0a1b0d0caa452ee263116fef930b
Hugo Moran <moranh56@gmail.com>
2022-07-02 12:45:00 -0600

    Add the cars load of the selected level

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,14 +1,18 @@
 import { KaboomCtx, SceneDef } from 'kaboom';
 import { BoardCoordinates, BoardMainMeasurements } from '../constants/board-constants';
 import { Board } from '../game-objects/Board';
+import { Car } from '../game-objects/Car';
 import { PuzzleLevel } from '../types/PuzzleLevel';
 import { Button } from '../ui/Button';
 import { SelectLevelScene } from './SelectLevelScene';
 
 export class GameScene {
     static readonly id: string = 'GameScene';
+    private cars: Car[];
 
-    constructor(private kaboomCtx: KaboomCtx) { }
+    constructor(private kaboomCtx: KaboomCtx) {
+        this.cars = [];
+    }
 
     readonly sceneDef: SceneDef = async(level?: PuzzleLevel) => {
         const board = new Board(this.kaboomCtx);
@@ -16,6 +20,12 @@ export class GameScene {
         const actionButtonPos = this.kaboomCtx.vec2(this.kaboomCtx.center().x, BoardCoordinates.Y + BoardMainMeasurements.BOARD_SIDE_LENGTH + 20);
 
         if (level) {
+            for (const levelCar of level.cars) {
+                const car = new Car(this.kaboomCtx, levelCar.carId, levelCar.x, levelCar.y, levelCar.orientation);
+                car.addGameObject();
+                this.cars.push(car);
+            }
+
             Button.addButton(
                 this.kaboomCtx,
                 'start-button',
