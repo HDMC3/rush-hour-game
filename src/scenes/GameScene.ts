8b36449b7be05c9c8667222476bd502aa37b49cc
Hugo Moran <moranh56@gmail.com>
2022-07-04 01:40:04 -0600

    Add time indicator

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -5,18 +5,23 @@ import { Car } from '../game-objects/Car';
 import { PuzzleLevel } from '../types/PuzzleLevel';
 import { Button } from '../ui/Button';
 import { SceneHeader } from '../ui/SceneHeader';
+import { TimeIndicator } from '../ui/TimeIndicator';
 import { SelectLevelScene } from './SelectLevelScene';
 
 export class GameScene {
     static readonly id: string = 'GameScene';
     private cars: Car[];
     private startButton: Button;
+    private pauseButton: Button;
     private selectLevelButton: Button;
+    private timeIndicator: TimeIndicator;
 
     constructor(private kaboomCtx: KaboomCtx) {
         this.cars = [];
         this.startButton = new Button(this.kaboomCtx);
+        this.pauseButton = new Button(this.kaboomCtx);
         this.selectLevelButton = new Button(this.kaboomCtx);
+        this.timeIndicator = new TimeIndicator(this.kaboomCtx);
     }
 
     readonly sceneDef: SceneDef = async(level?: PuzzleLevel) => {
@@ -25,7 +30,12 @@ export class GameScene {
 
         const board = new Board(this.kaboomCtx);
         await board.addGameObject();
-        const startButtonPos = this.kaboomCtx.vec2(this.kaboomCtx.center().x, BoardCoordinates.Y + BoardMainMeasurements.BOARD_SIDE_LENGTH + 20);
+
+        const timeIndicatorPos = this.kaboomCtx.vec2(50, BoardCoordinates.Y);
+        this.timeIndicator.addTimeIndicator(timeIndicatorPos);
+
+        const startButtonPos = this.kaboomCtx.vec2(this.kaboomCtx.center().x - BoardMainMeasurements.BOARD_SIDE_LENGTH / 4, BoardCoordinates.Y + BoardMainMeasurements.BOARD_SIDE_LENGTH + 20);
+        const pauseButtonPos = this.kaboomCtx.vec2(this.kaboomCtx.center().x + BoardMainMeasurements.BOARD_SIDE_LENGTH / 4, BoardCoordinates.Y + BoardMainMeasurements.BOARD_SIDE_LENGTH + 20);
         const selectLevelButtonPos = this.kaboomCtx.vec2(this.kaboomCtx.center().x, BoardCoordinates.Y + BoardMainMeasurements.BOARD_SIDE_LENGTH + 20);
 
         if (level) {
@@ -41,8 +51,26 @@ export class GameScene {
                 startButtonPos,
                 'start-button-sprite',
                 () => {
+                    this.timeIndicator.start();
+                    this.pauseButton.enable();
+                    this.startButton.disable();
+                    this.selectLevelButton.disable();
                 }
             );
+
+            this.pauseButton.addButton(
+                'pause-button',
+                'top',
+                pauseButtonPos,
+                'pause-button-sprite',
+                () => {
+                    this.timeIndicator.pause();
+                    this.startButton.enable();
+                    this.pauseButton.disable();
+                    this.selectLevelButton.enable();
+                }
+            );
+            this.pauseButton.disable();
 
             selectLevelButtonPos.y += 50;
         }
@@ -55,6 +83,7 @@ export class GameScene {
             () => {
                 this.clearCars();
                 this.clearButtons();
+                this.timeIndicator.destroy();
                 this.kaboomCtx.go(SelectLevelScene.id);
             }
         );
---
+++ src/ui/TimeIndicator.ts
@@ -0,0 +1,74 @@
+import { GameObj, KaboomCtx, OriginComp, PosComp, SpriteComp, TextComp, Vec2 } from 'kaboom';
+
+export class TimeIndicator {
+
+    private active: boolean;
+    private minutes: number;
+    private seconds: number;
+    private gameObject?: GameObj<SpriteComp | OriginComp | TextComp | PosComp>;
+    private interval?: number;
+
+    constructor(private kaboomCtx: KaboomCtx) {
+        this.active = false;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+
+    addTimeIndicator(position: Vec2) {
+
+        position.x += 192 / 2;
+        position.y += 130 / 2;
+        this.gameObject = this.kaboomCtx.add([
+            this.kaboomCtx.sprite('time-and-moves-indicator-sprite'),
+            this.kaboomCtx.origin('center'),
+            this.kaboomCtx.text(' TIEMPO\n00:00', { font: 'custom-font', size: 27, lineSpacing: 10 }),
+            this.kaboomCtx.pos(position)
+        ]);
+    }
+
+    start() {
+        this.interval = setInterval(() => {
+            if (++this.seconds === 60) {
+                this.seconds = 0;
+                this.minutes++;
+            }
+            if (this.gameObject) {
+                const secondsText = this.seconds < 10 ? `0${this.seconds}` : this.seconds;
+                const minutesText = this.minutes < 10 ? `0${this.minutes}` : this.minutes;
+                this.gameObject.text = ` TIEMPO\n${minutesText}:${secondsText}`;
+            }
+        }, 1000);
+        this.active = true;
+    }
+
+    pause() {
+        this.clearInterval();
+        this.active = false;
+    }
+
+    stop() {
+        this.clearInterval();
+        this.active = false;
+        this.seconds = 0;
+        this.minutes = 0;
+        if (this.gameObject) this.gameObject.text = ' TIEMPO\n00:00';
+    }
+
+    destroy() {
+        if (this.gameObject) {
+            this.stop();
+            this.gameObject.destroy();
+        }
+    }
+
+    get isActive() {
+        return this.active;
+    }
+
+    private clearInterval() {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = undefined;
+        }
+    }
+}
