f1ddd7975292e640fe5091ec905fb57aa44e8fb8
Hugo Moran <moranh56@gmail.com>
2022-07-13 13:08:43 -0600

    Fix Vite config to Github Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfigExport } from 'vite';
+import path from 'path';
 
-export default defineConfig({
-    root: 'src',
-    publicDir: '../public',
-    server: {
-        host: '0.0.0.0'
-    },
-    build: {
-        outDir: '../dist',
-        assetsDir: './'
-    }
-})
+export default defineConfig(({ mode }) => {
+    const config: UserConfigExport = {
+        root: 'src',
+        publicDir: '../public',
+        server: {
+            host: '0.0.0.0'
+        },
+        build: {
+            outDir: '../dist',
+            assetsDir: './'
+        }
+    };
+
+    config.base = mode === 'production' ? '/' + path.basename(process.cwd()) + '/' : '/';
+    return config;
+});
